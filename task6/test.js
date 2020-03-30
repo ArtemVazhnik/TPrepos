c5753ff99891207d9a5ad49f5f3b4920cc8f7cae
ArtemVazhnik <milten.art@gmail.com>
2020-03-30 18:15:31 +0300

    task6 added

---
+++ task6/test.js
@@ -0,0 +1,436 @@
+var posts = [
+
+  {
+   id: '1',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-17T23:00:00'),
+   author: 'Иванов Иван',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#hashtag1', '#hashtag2', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '2',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-18T15:42:05'),
+   author: 'Максимов Максим',
+   hashTags: ['#hashtag1', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '3',
+   description: 'Привет. ',
+   createdAt: new Date('2020-03-18T23:40:07'),
+   author: 'Иванов Иван',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '4',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-20T20:15:43'),
+   author: 'Иванов Иван',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#hashtag1', '#hashtag2', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '5',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-15T03:00:00'),
+   author: 'Ольг Викторов',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#hashtag1', '#hashtag2', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '6',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2000-03-17T23:00:00'),
+   author: 'Иванов Иван',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '7',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2021-05-17T23:04:50'),
+   author: 'Иван Дулин',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '8',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2021-03-17T23:47:49'),
+   author: 'ManInBlack',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '9',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2008-03-17T10:00:00'),
+   author: 'Иванов Иван',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '10',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-17T23:00:00'),
+   author: 'Иванов Иван',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#hashtag1', '#hashtag2', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '11',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-17T23:00:00'),
+   author: 'Иванов Иван',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '12',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-02-17T20:10:04'),
+   author: 'Press F',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '13',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-17T22:40:01'),
+   author: 'oh my',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '14',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-17T23:00:00'),
+   author: 'Иванов Иван',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '15',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2000-01-17T23:00:00'),
+   author: 'Igor Ivanovich',
+   hashTags: ['#hashtag1', '#hashtag2', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '16',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2025-03-17T23:20:20'),
+   author: 'Иванов Кирилл',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '17',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-17T23:20:00'),
+   author: 'пщфышра',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '18',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-03-17T13:00:20'),
+   author: 'фтоф фдла',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '19',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2000-03-17T23:06:00'),
+   author: 'фафф ф ф ф',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+  {
+   id: '20',
+   description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+   createdAt: new Date('2020-05-17T23:00:00'),
+   author: 'Goro',
+   hashTags: ['#hashtag1', '#hashtag2', '#sport', '#EA', '#PS4'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  },
+
+];
+
+(function() {
+
+  testPost = {
+   description: 'HI ',
+   createdAt: new Date('1990-03-18T23:40:07'),
+   author: 'name surname',
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   //hashTags: ['#hashtag1', '#hashtag2', 'sport', 'EA', 'PS4'],
+   hashTags: ['#hashtag2', '#sport'],
+   likes: ['Виктор Комаров', 'Antony Pavlov', 'Gorrive Curipita']
+  }
+
+  function getPostNumber(id) {
+    for (var i = 0; i < posts.length; i++) {
+      if (id === posts[i].id) {
+        return i;
+      }
+    }
+  }
+
+  function validatePost(post) {
+    if (typeof post.id !== 'string') {
+      console.log("Invalid post!");
+      return false;
+    }
+    if (typeof post.description !== 'string') {
+      console.log("Invalid post!");
+      return false;
+    }
+    if (typeof post.createdAt !== 'object') {
+      console.log("Invalid post!");
+      return false;
+    }
+    if (typeof post.author !== 'string') {
+      console.log("Invalid post!");
+      return false;
+    }
+    if (typeof post.likes !== 'object') {
+      console.log("Invalid post!");
+      return false;
+    }
+    console.log("Valid post");
+    return true;
+  }
+
+  function getPost(id) {   
+    // for (var i of posts) {
+    //   if (id === i.id)  {
+    //     console.log(i);
+    //     return i;
+    //   }
+    // } 
+    var postNumber = getPostNumber(id);
+    if (postNumber !== undefined) {
+      console.log(posts[postNumber]);
+      return posts[postNumber];
+    } 
+    console.log("No such post!!!");  
+  }
+
+  function addPost(post) {
+    post.id = (parseInt(posts[posts.length - 1].id, 10) + 1 + "");
+    if (validatePost(post) === false) {
+      console.log("Adding post fault!");
+      return false;
+    }
+    posts.push(post);
+    console.log("Post added with ID: " + post.id);
+    return true;
+  }
+
+  function editPost(id, post) {
+    postNumber = getPostNumber(id);
+    if (postNumber === undefined) {
+      console.log("No such post!!!");
+      return false;
+    } 
+    if (validatePost(posts[postNumber]) === false) {
+      console.log("Invalid post in array!");
+      return false;
+    }
+
+    if (typeof post.description === 'string') {
+      posts[postNumber].description = post.description;
+    }
+    else if (typeof post.description !== 'undefined') {
+      console.log("Invalid post edition!!! (description)");
+      return false;
+    }
+
+    if (typeof post.photoLink === 'string') {
+      posts[postNumber].photoLink = post.photoLink;
+    }
+    else if (typeof post.photoLink !== 'undefined') {
+      console.log("Invalid post edition!!! (photoLink)");
+      return false;
+    }
+
+    if (typeof post.hashTags === 'object') {
+      posts[postNumber].hashTags = post.hashTags;
+    }
+    else if (typeof post.hashTags !== 'undefined') {
+      console.log("Invalid post edition!!! (hashTags)");
+      return false;
+    }
+
+    if (typeof post.likes === 'object') {
+      posts[postNumber].likes = post.likes;
+    }
+    else if (typeof post.likes !== 'undefined') {
+      console.log("Invalid post edition!!! (likes)");
+      return false;
+    }
+
+    return true;
+  }
+
+  function removePost(id) {
+    for (var i = 0; i < posts.length; i++) {
+      if (id === posts[i].id) {
+        console.log("Post deleted");
+        posts.splice(i, 1);
+        return true;
+      }
+    }
+    console.log("No such post to delete!!!");
+    return false;
+  }
+
+
+
+  function getPosts(skip, top, filterConfig) {
+    let somePosts = posts;
+    if (typeof filterConfig.author === 'string') {
+      somePosts = posts.filter(item => item.author === filterConfig.author);
+    }
+    if (typeof filterConfig.createdAt === 'object') {
+      somePosts = somePosts.filter(item => item.createdAt === filterConfig.createdAt);
+    }
+
+    if (typeof filterConfig.hashTags === 'object') {
+      valid = true;
+      for (let i of filterConfig.hashTags) {
+        if (typeof i !== 'string') {
+          valid = false;
+          break;
+        }
+      }
+      if (valid === true) {
+        somePosts = somePosts.filter(function(item){
+          answer = true;
+          for (let i of filterConfig.hashTags) {
+            if (item.hashTags.includes(i) === false) {
+              answer = false;
+              break;
+            }
+          }
+          return answer;
+        });
+      }
+    }
+
+    somePosts.sort(function(a, b){
+      return b.createdAt - a.createdAt; 
+    })
+
+    somePosts = somePosts.slice(skip, skip + top);
+
+
+    for (let i of somePosts) {
+      console.log(i);
+    }
+
+    return somePosts;
+  }
+
+
+  console.log(".....validatePost testing.....");
+  console.log("\n");
+  console.log("with valid post:");
+  validatePost(posts[0]);
+  console.log("\n");
+  console.log("with invalid post:");
+  validatePost(1);
+  console.log("\n");
+  console.log("\n");
+
+  console.log(".....getPost testing.....");
+  console.log("\n");
+  console.log("with valid id:");
+  getPost('3');
+  console.log("\n");
+  console.log("with invalid id:");
+  getPost(0);
+  console.log("\n");
+  console.log("\n");
+
+  console.log(".....addPost testing.....");
+  console.log("\n");
+  console.log("with valid post:");
+  addPost(testPost);
+  console.log("\n");
+  console.log("with invalid post:");
+  addPost(0);
+  console.log("\n");
+  console.log("\n");
+
+  console.log(".....editPost testing.....");
+  console.log("\n");
+  console.log("with valid post and ID:");
+  editPost("1", testPost);
+  console.log("\n");
+  console.log("with invalid post:");
+  editPost("1", {description: 1441});
+  console.log("\n");
+  console.log("with invalid ID:");
+  editPost(0, testPost);
+  console.log("\n");
+  console.log("\n");
+
+  console.log(".....removePost testing.....");
+  console.log("\n");
+  console.log("with valid ID:");
+  removePost("20");
+  console.log("\n");
+  console.log("with invalid ID:");
+  removePost(30);
+  console.log("\n");
+  console.log("\n");
+
+  console.log("..... getPosts testing.....");
+  console.log("\n");
+  console.log("1st valid:");
+  getPosts(0, 10, {author: 'Иванов Иван'});
+  console.log("\n");
+  console.log("2nd valid:");
+  getPosts(0, 10, {author: 'Иванов Иван',
+                   hashTags: ['#sport', '#EA']});
+  console.log("\n");
+  console.log("\n");
+
+  
+}());
